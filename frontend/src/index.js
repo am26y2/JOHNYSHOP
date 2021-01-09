6425b8347307fef8bc7a569ea2c0243878a046aa
am26y2 <amitraj9046@gmail.com>
2021-01-09 17:14:18 +0530

    change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./bootstrap.min.css";
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- frontend/src/screens/homeScreen.jsx
+++ frontend/src/screens/homeScreen.jsx
@@ -1,29 +1,31 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from 'axios';
+import { listProducts } from "../Actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+
 const HomeScreen = () => {
-  const [loading,setLoading ]=useState(false);
-  const [products,setProducts]=useState([])
-  useEffect(()=>{
-    setLoading(true);
-    const fetchProduct=async ()=>{
-      const {data}=await axios.get('./api/products')
-      setProducts(data)
-      setLoading(false)
-    }
-    fetchProduct()
-  },[])
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList); //grab product data from state
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts()); //firing of the action to get the products and send it through the reducer down into the state
+  }, [dispatch]); //to avoid warning in console
 
   return (
     <>
-      <h1>Latest Products</h1>
       {loading ? (
-        <h1>Loading...</h1>
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((el) => (
-            <Col sm={12} lg={4} xl={3}>
+            <Col sm={12} md={6} lg={4} xl={3}>
               <Product product={el} />
               {console.log(el)}
             </Col>
