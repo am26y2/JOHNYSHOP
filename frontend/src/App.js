9ff2111b045d7ec6fe4f89da3d2d084504f74194
am26y2 <amitraj9046@gmail.com>
2021-01-10 09:53:55 +0530

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Header />
-      <main class Name="py-3">
+      <main className="py-3">
         <Container>
           <Route  path={`/product/:id`} component={ProductScreen}/>
           <Route exact path='/' component={HomeScreen}/>
